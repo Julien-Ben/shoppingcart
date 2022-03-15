11b2f631159199df8e64562e857df3ba05f60455
Julien-Ben <33035980+Julien-Ben@users.noreply.github.com>
2022-03-15 11:07:34 +0100

    Refactoring unit tests

--- java/src/test/java/com/xgen/interview/ShoppingCartTest.java
+++ java/src/test/java/com/xgen/interview/ShoppingCartTest.java
@@ -15,27 +15,15 @@ public class ShoppingCartTest {
     @Test
     public void canAddAnItem() {
         ShoppingCart sc = new ShoppingCart(new Pricer());
-
         sc.addItem("apple", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-        assertEquals(String.format("apple - 1 - €1.00%n"), myOut.toString());
+        assertReceiptHasExpectedContent(sc, "apple - 1 - €1.00%n");
     }
 
     @Test
     public void canAddMoreThanOneItem() {
         ShoppingCart sc = new ShoppingCart(new Pricer());
-
         sc.addItem("apple", 2);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-        assertEquals(String.format("apple - 2 - €2.00%n"), myOut.toString());
+        assertReceiptHasExpectedContent(sc, "apple - 2 - €2.00%n");
     }
 
     @Test
@@ -62,14 +50,16 @@ public class ShoppingCartTest {
         @Test
     public void doesntExplodeOnMysteryItem() {
         ShoppingCart sc = new ShoppingCart(new Pricer());
-
         sc.addItem("crisps", 2);
+        assertReceiptHasExpectedContent(sc, "crisps - 2 - €0.00%n");
+    }
 
+    private void assertReceiptHasExpectedContent(ShoppingCart sc, String content) {
         final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
         sc.printReceipt();
-        assertEquals(String.format("crisps - 2 - €0.00%n"), myOut.toString());
+        assertEquals(String.format(content), myOut.toString());
     }
 }
 
